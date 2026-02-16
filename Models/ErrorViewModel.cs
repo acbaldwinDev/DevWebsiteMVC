8739b9d77a331965825f078e426371a4c38c9b5b
alexb <acbaldwin72@gmail.com>
2026-02-16 09:52:13 -0800

    Initial commit - ASP.NET Core MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DevWebsiteMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
