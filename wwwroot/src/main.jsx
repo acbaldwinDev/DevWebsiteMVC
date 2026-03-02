3e3807cd71fc97e04dc2d971915e9c010d353e01
alexb <acbaldwin72@gmail.com>
2026-03-02 12:58:48 -0800

    Add an api controller

---
+++ wwwroot/src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+    return <h2>Hello from React!</h2>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
