fe58fade8b1d1523b65f0f61433593b89ce4e661
alexb <acbaldwin72@gmail.com>
2026-03-02 13:30:14 -0800

    angular libary

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module("myApp", []);
+
+app.controller("homeController", function ($scope, $http) {
+
+    $scope.message = "Hello from AngularJS!";
+
+    $scope.getData = function () {
+        $http.get("/Home/GetMessage")
+            .then(function (response) {
+                $scope.serverMessage = response.data;
+            });
+    };
+});
